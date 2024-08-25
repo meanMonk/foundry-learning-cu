4b0a5c167aaf791cc3a96d0388788557e9aa854a
Sahil Kashetwar <sahilkashetwar24@gmail.com>
2024-08-25 21:17:16 +0530

    - sample test file - sample script for deployFundMe contract `forge script script/<name>.s.sol`

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from '../src/FundMe.sol';
+
+contract FundMeTest is Test {
+  FundMe fundMe;
+  
+  function setUp() external {
+    // us (caller) => FundMeTest (contract owner) => FundMe.
+    fundMe = new FundMe();
+  } 
+  
+  function testMinimumDollarIsFive() public view {
+    assertEq(fundMe.MINIMUM_USD(), 5e18);
+  }
+  
+  function testOwnerIsMsgOwner() public view {
+    assertEq(fundMe.i_owner(), address(this));
+  }
+}
