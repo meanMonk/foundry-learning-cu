7a03a44728ab942b112a5589e1ab778df65a3f4b
Sahil Kashetwar <sahilkashetwar24@gmail.com>
2024-08-26 19:35:56 +0530

    learn [hoax] to create and fund address. lean [startPrank] [stopprank] for pranking. assertion on multiple funders withdraw.

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -96,7 +96,33 @@ contract FundMeTest is Test {
     
   }
   
+  function testWithdrawFromMultipleFunders() public funded {
+    uint160 numberOfFunders = 10;
+    uint160 startingFunderIndex = 1;
+    
+    // to avoid the sending fund at 0 address set starting at 1.
+    for( uint160 i = startingFunderIndex; i < numberOfFunders ;i++) {
+      // vm prank new address.
+      // vm deal add balance.
+      
+      hoax(address(i), SEND_VALUE);
+      fundMe.fund{value: SEND_VALUE}();
+      // fund the fundMe.
+    }
+    
+    uint256 startingOwnerBalance = fundMe.getOwner().balance;
+    uint256 startingFundMeBalance = address(fundMe).balance;
+    console.log(startingFundMeBalance);
+    // start and stop prank.
+    vm.startPrank(fundMe.getOwner());
+    fundMe.withdraw();
+    vm.stopPrank();
   
+    assertEq(address(fundMe).balance, 0);
+    
+    assertEq(startingFundMeBalance + startingOwnerBalance, fundMe.getOwner().balance);
+    
+  }
   
   
 }
